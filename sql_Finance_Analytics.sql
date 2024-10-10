4b250b6979c9477ec0527286e40ea8dd07c8dcad
sruthi1899 <125146134+sruthi1899@users.noreply.github.com>
2024-10-10 17:39:00 -0400

    Add files via upload

---
+++ sql_Finance_Analytics.sql
@@ -0,0 +1,116 @@
+-- a. Perform joins to pull product information
+	SELECT s.date, s.product_code, p.product, p.variant, s.sold_quantity 
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+        ON s.product_code=p.product_code
+	WHERE 
+            customer_code=90002002 AND 
+    	    get_fiscal_year(date)=2021     
+	LIMIT 1000000;
+
+-- b. Performing join with 'fact_gross_price' table with the above query and generating required fields
+	SELECT 
+    	    s.date, 
+            s.product_code, 
+            p.product, 
+            p.variant, 
+            s.sold_quantity, 
+            g.gross_price,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+            ON g.fiscal_year=get_fiscal_year(s.date)
+    	AND g.product_code=s.product_code
+	WHERE 
+    	    customer_code=90002002 AND 
+            get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+
+-- Generate monthly gross sales report for Croma India for all the years
+	SELECT 
+            s.date, 
+    	    SUM(ROUND(s.sold_quantity*g.gross_price,2)) as gross_price_total
+	FROM fact_sales_monthly s
+	JOIN fact_gross_price g
+        ON g.fiscal_year=get_fiscal_year(s.date) AND g.product_code=s.product_code
+	WHERE 
+             customer_code=90002002
+	GROUP BY date;
+
+	select
+            get_fiscal_year(date) as fiscal_year,
+            sum(round(sold_quantity*g.gross_price,2)) as yearly_sales
+	from fact_sales_monthly s
+	join fact_gross_price g
+	on 
+	    g.fiscal_year=get_fiscal_year(s.date) and
+	    g.product_code=s.product_code
+	where
+	    customer_code=90002002
+	group by get_fiscal_year(date)
+	order by fiscal_year;
+    
+ -- generate net_invoice_sales
+ SELECT 
+            *,
+    	    (gross_price_total-pre_invoice_discount_pct*gross_price_total) as net_invoice_sales
+	FROM gdb0041.sales_preinv_discount;
+-- find out customer wise net sales percentage contribution 
+	with cte1 as (
+		select 
+                    customer, 
+                    round(sum(net_sales)/1000000,2) as net_sales_mln
+        	from net_sales s
+        	join dim_customer c
+                    on s.customer_code=c.customer_code
+        	where s.fiscal_year=2021
+        	group by customer)
+	select 
+            *,
+            net_sales_mln*100/sum(net_sales_mln) over() as pct_net_sales
+	from cte1
+	order by net_sales_mln desc;
+    
+-- Find customer wise net sales distibution per region for FY 2021
+	with cte1 as (
+		select 
+        	    c.customer,
+                    c.region,
+                    round(sum(net_sales)/1000000,2) as net_sales_mln
+                from gdb0041.net_sales n
+                join dim_customer c
+                    on n.customer_code=c.customer_code
+		where fiscal_year=2021
+		group by c.customer, c.region)
+	select
+             *,
+             net_sales_mln*100/sum(net_sales_mln) over (partition by region) as pct_share_region
+	from cte1
+	order by region, pct_share_region desc;
+
+-- Find out top 3 products from each division by total quantity sold in a given year
+	with cte1 as 
+		(select
+                     p.division,
+                     p.product,
+                     sum(sold_quantity) as total_qty
+                from fact_sales_monthly s
+                join dim_product p
+                      on p.product_code=s.product_code
+                where fiscal_year=2021
+                group by p.product,p.division),
+           cte2 as 
+	        (select 
+                     *,
+                     dense_rank() over (partition by division order by total_qty desc) as drnk
+                from cte1)
+	select * from cte2 where drnk<=3;
+
+
+
+    
+    
+
+	
